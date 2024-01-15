b4022cfb2a956ebebaa79474892fd78b79ba75ea
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-15 18:39:35 +0530

    Add files via upload

---
+++ sortAnArryas.java
@@ -0,0 +1,10 @@
+package ArraysProgram;
+
+import java.util.Arrays;
+
+public class sortAnArryas {
+    public static void main(String []args){
+        int a[]=   {1, 4,5,7,2,4,9,0,3};
+        System.out.println(Arrays.toString(a));
+    }
+}
