f52e7b49c8e6fb011da7ec4a4002e62c41e1ca5f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-17 11:29:50 +0530

    Add files via upload

---
+++ smallestElement.java
@@ -0,0 +1,21 @@
+package ArraysProgram;
+
+public class smallestElement {
+    public static void main(String []args){
+        int arr[]={ 3,4,9,0,-1};
+       int ans= smallestelemttt(arr, arr.length);
+       System.out.println(ans);
+    }
+    public static int smallestelemttt(int arr[] , int n){
+        int min=Integer.MAX_VALUE;
+        for(int i=0; i<n; i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+}
+
+
+
