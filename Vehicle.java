06d1c34d70272e7d518c93de6aee88dac32783a6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-23 18:52:27 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,21 @@
+package abstractInJava;
+
+public  abstract class Vehicle {
+    private int noOfTires;
+    public abstract  void  makeStartSound();
+
+    public Vehicle(int noOfTires) {
+        this.noOfTires = noOfTires;
+    }
+
+    public int getNoOfTires() {
+        return noOfTires;
+    }
+
+    public void setNoOfTires(int noOfTires) {
+        this.noOfTires = noOfTires;
+    }
+    public void commute(){
+        System.out.println(" going ...");
+    }
+}
