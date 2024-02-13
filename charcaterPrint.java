b91f0d62cf1870d9862deba2bd082f746a55440a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-13 20:09:58 +0530

    Add files via upload

---
+++ charcaterPrint.java
@@ -0,0 +1,12 @@
+package StringInJava;
+
+public class charcaterPrint {
+    public static void main(String []args){
+        String str=" vishalRai";
+        char ch= str.charAt(3);
+        System.out.println(ch+" ");
+
+
+
+    }
+}
