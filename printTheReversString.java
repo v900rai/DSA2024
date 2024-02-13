03a63718017e5d4aa32dd7d13c869a01c929232f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-13 20:19:49 +0530

    Add files via upload

---
+++ printTheReversString.java
@@ -0,0 +1,10 @@
+package StringInJava;
+
+public class printTheReversString {
+    public static void main(String[] args) {
+        String s="Dingaa";
+        for (int i = s.length(); i>0;  i--) {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
