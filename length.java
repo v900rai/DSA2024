c2bd8be446744e2d75ec9cd8d6b4d2c5ba0907b0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-13 20:10:56 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,11 @@
+package StringInJava;
+
+public class length {
+    public static void main(String []args){
+        String str="Vishal Rai";
+       // int l=str.length();
+        // find the length of the character in the given
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+    }
+}
