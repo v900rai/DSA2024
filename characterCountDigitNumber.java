64721359d78b64377876fa25c795023ed6cd241e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-13 20:15:41 +0530

    Add files via upload

---
+++ characterCountDigitNumber.java
@@ -0,0 +1,11 @@
+package StringInJava;
+
+public class characterCountDigitNumber {
+    public static void main(String[] args) {
+        String st="DingaAndZingaBeta";
+        for (int i = 0; i <=st.length()-1; i++) {
+            System.out.print(st.charAt(i)+" ");
+
+        }
+    }
+}
