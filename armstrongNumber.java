d011a65fa112a6c1270ea7e0aa8fced25ee5ecbc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-14 14:15:13 +0530

    Add files via upload

---
+++ armstrongNumber.java
@@ -0,0 +1,14 @@
+package JavaInterViewProgram100VVI;
+
+public class armstrongNumber {
+    public static void main(String []args){
+        //153: 13 + 53 + 33 = 1 + 125+ 27 = 153
+        //
+        //125: 13 + 23 + 53 = 1 + 8 + 125 = 134 (Not an Armstrong Number)
+        //
+        //1634: 14 + 64 + 34 + 44 = 1 + 1296 + 81 + 256 = 1643
+        //
+        //Similarly, we can check other number also.
+
+    }
+}
