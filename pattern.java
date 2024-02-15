c7420736374df9d77e1c63aa594cadc8c9d474cf
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-15 13:18:23 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern {
+  public static void main(String[] args) {
+    int num = 10;
+    for (int i = 1; i < num; i++) {
+      for (int j = i; j < num; j++) {
+        System.out.print(i + " ");
+
+      }
+      System.out.println();
+    }
+  }
+
+}
