d2f3413afaa288e40c6ac17db62b04de06f223e7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-16 17:15:06 +0530

    user input forget
    i create an aarays span but i forget taking user input from the aarays but

--- spanArrays.java
+++ spanArrays.java
@@ -7,7 +7,7 @@ public class spanArrays {
     int n = sc.nextInt();
     int arr[] = new int[n];
     for (int i = 0; i < arr.length; i++) {
-      System.out.println(arr[i]);
+      arr[i] = sc.nextInt();
     }
     int max = arr[0];
     int min = arr[0];
