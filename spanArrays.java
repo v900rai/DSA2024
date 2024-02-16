651be488049830a45d5e6dc2671ddb3dbd6eaa5d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-16 17:00:18 +0530

    Add files via upload

---
+++ spanArrays.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class spanArrays {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number max and min");
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[1];
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+    int span = max - min;
+    System.out.println(span);
+  }
+
+}
