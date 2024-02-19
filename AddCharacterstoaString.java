b2865eb942e341cd1f7479cad90f72ffa85aaddd
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-19 19:03:08 +0530

    Add files via upload

---
+++ AddCharacterstoaString.java
@@ -0,0 +1,11 @@
+package JavaInterViewProgram100VVI;
+
+public class AddCharacterstoaString {
+    public static void main(String[] args) {
+        char ch='r';
+        String str="vishal";
+        String str2=str+ch;
+        System.out.println(str2);
+
+    }
+}
