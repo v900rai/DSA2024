79399dec158bb76b54afb39f0eb50674c4e2f156
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-20 18:32:23 +0530

    Add files via upload

---
+++ mainOops.java
@@ -0,0 +1,10 @@
+package JavaInterViewProgram100VVI;
+
+public class mainOops {
+    int x=10;
+
+    public static void main(String[] args) {
+        mainOops obj=new mainOops();
+        System.out.println(obj.x);
+    }
+}
