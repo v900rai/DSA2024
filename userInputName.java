91411f664da0e734289bdbd68c20b28bf305c893
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-20 19:02:32 +0530

    Add files via upload

---
+++ userInputName.java
@@ -0,0 +1,13 @@
+package JavaInterViewProgram100VVI;
+
+
+import java.util.Scanner;
+
+public class userInputName {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        String userName=sc.nextLine();
+        System.out.println(" username  "+userName);
+    }
+}
