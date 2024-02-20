1296834755e138f3444aa24d99e912135d9501b1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-20 18:38:16 +0530

    Add files via upload

---
+++ oopsMethods.java
@@ -0,0 +1,16 @@
+package JavaInterViewProgram100VVI;
+
+public class oopsMethods {
+    static  void myMethod(){
+        System.out.println("hellp world!");
+    }
+    static int mysum(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+       int sum= mysum(1,2);
+        System.out.println(sum);
+    }
+}
