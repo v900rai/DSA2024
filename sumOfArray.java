66592f4914b8d0bbecfcde7b3d3277a79ef4caab
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-21 19:25:01 +0530

    Add files via upload

---
+++ sumOfArray.java
@@ -0,0 +1,13 @@
+public class sumOfArray {
+  public static void main(String[] args) {
+    int arr[] = { 10, 12, 13, 40 };
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+      if (arr[i] % 2 == 0) {
+        System.out.println("even Number");
+      }
+
+    }
+  }
+
+}
