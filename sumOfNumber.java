08d850d198bc9300615a70690852218616b78da3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-22 19:41:37 +0530

    Add files via upload

---
+++ sumOfNumber.java
@@ -0,0 +1,11 @@
+public class sumOfNumber {
+  public static void main(String[] args) {
+    int sum = 0;
+    int num = 10;
+    for (int i = 1; i < num; i++) {
+      sum += i;
+    }
+    System.out.println(sum + " " + num);
+  }
+
+}
