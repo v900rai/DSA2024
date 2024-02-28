e4e3a1e12994b2e155e284628a396ecfb22fb426
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-28 20:34:25 +0530

    Add files via upload

--- patternProgramReversStrrr.java
+++ patternProgramReversStrrr.java
@@ -3,7 +3,7 @@ package PatternInJava;
 public class patternProgramReversStrrr {
     public static void main(String[] args) {
         for(int i=5; i>=1; i--){
-            for(int j=1; j<=5;  j++){
+            for(int j=1; j<=i;  j++){
                 System.out.print("*");
             }
             System.out.println();
