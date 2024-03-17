8e19bb5918bcb497fb9336bfb5efbe987d6bafb2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-17 20:38:36 +0530

    Add files via upload

---
+++ loopingArrays.java
@@ -0,0 +1,13 @@
+package ArraysProgram;
+
+public class loopingArrays {
+    public static void main(String[] args) {
+        int[] intArray = {21, 15, 37, 53, 17};
+
+        for (int i = 0; i < intArray.length; i++)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
