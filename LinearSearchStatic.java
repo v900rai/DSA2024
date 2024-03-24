2dbf5ab187c73c864a80b0c95051bbcc94ea1a08
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-24 19:36:06 +0530

    Add files via upload

---
+++ LinearSearchStatic.java
@@ -0,0 +1,17 @@
+package SearchingInDsaJava;
+
+public class LinearSearchStatic {
+    public static void main(String []args){
+        int []arr={10,20,30,40,50,60};
+        int key=30;
+        System.out.println(linearSearch(arr,key));
+    }
+    public static int linearSearch(int []arr, int key){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
