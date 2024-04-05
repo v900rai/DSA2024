dbcc42b46688c4bdebd6042054f3d1c9295fe685
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-05 23:49:27 +0530

    Add files via upload

---
+++ TimeAndDate.java
@@ -0,0 +1,22 @@
+package Hashmap;
+import java.util.*;
+public class TimeAndDate {
+
+
+        public static void main(String[] args)
+        {
+            // Create a default calendar
+            Calendar cal = Calendar.getInstance();
+            // Get and display information of current date from the calendar:
+            System.out.println();
+            System.out.println("Year: " + cal.get(Calendar.YEAR));
+            System.out.println("Month: " + cal.get(Calendar.MONTH));
+            System.out.println("Day: " + cal.get(Calendar.DATE));
+            System.out.println("Hour: " + cal.get(Calendar.HOUR));
+            System.out.println("Minute: " + cal.get(Calendar.MINUTE));
+            System.out.println("Second: " + cal.get(Calendar.SECOND));
+            System.out.println();
+        }
+    }
+
+
