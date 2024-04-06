ecfd6c5a77692e03484b4b1a74c68085bc36bd4b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-06 19:43:20 +0530

    Add files via upload

---
+++ sumArrays.java
@@ -0,0 +1,12 @@
+package ExcepationHandlingInJava;
+
+public class sumArrays {
+    public static void main(String []args){
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int sum=0;
+        for(int i : arr){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
