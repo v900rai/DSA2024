f263869e2194d07544f129451ac2118103dcc7f8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-14 19:47:59 +0530

    Add files via upload

---
+++ LoginS.java
@@ -0,0 +1,17 @@
+package selfpractices;
+
+import javax.swing.*;
+
+public class LoginS {
+    public static void main(String[] args) {
+        JFrame j1= new JFrame("Welcome to facebook");
+        j1.setSize(500,500);
+        j1.setVisible(true);
+        j1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+
+
+
+    }
+}
